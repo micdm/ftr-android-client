5aba902843ef680cc3aaf82d00cb54dd50c19e5a
Mic <mic-dm@mail.ru>
2011-09-29 00:19:14 +0700

    fix: приводим в порядок экран с группами

--- src/info/micdm/ftr/activities/ForumActivity.java
+++ src/info/micdm/ftr/activities/ForumActivity.java
@@ -2,35 +2,36 @@ package info.micdm.ftr.activities;
 
 import info.micdm.ftr.Forum;
 import info.micdm.ftr.Group;
-import info.micdm.ftr.R;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 /**
  * Экран с группами тем.
  * @author Mic, 2011
  *
  */
-public class ForumActivity extends Activity {
+public class ForumActivity extends ListActivity {
 
 	/**
 	 * Заполняет список групп.
 	 */
-	protected void _setData(ListView list) {
+	protected void _setData() {
 		ArrayList<Group> groups = Forum.getInstance().getAllGroups();
-		ArrayAdapter<Group> adapter = new ArrayAdapter<Group>(this, R.layout.list_item, groups);
-		list.setAdapter(adapter);
+		ArrayAdapter<Group> adapter = new ArrayAdapter<Group>(this, android.R.layout.simple_list_item_1, groups);
+		getListView().setAdapter(adapter);
 	}
 	
+	/**
+	 * Выполняется, когда пользователь выбрал определенную группу.
+	 */
 	protected void _onGroupSelected(Group group) {
 		Log.d(toString(), "group \"" + group.getTitle() + "\" (" + group.getId() + ") selected");
 		Intent intent = new Intent(this, GroupActivity.class);
@@ -41,11 +42,11 @@ public class ForumActivity extends Activity {
 	/**
 	 * Слушает клик по элементу списка, чтобы перейти в соответствующую группу.
 	 */
-	protected void _listenForClick(final ListView list) {
-		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	protected void _listenForClick() {
+		getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-				Group group = (Group)list.getItemAtPosition(position);
+				Group group = (Group)adapter.getItemAtPosition(position);
 				_onGroupSelected(group);
 			}
 		});
@@ -55,15 +56,13 @@ public class ForumActivity extends Activity {
 	 * Отображает список групп.
 	 */
 	protected void _showGroups() {
-		ListView list = (ListView)findViewById(R.id.groups);
-		_setData(list);
-		_listenForClick(list);
+		_setData();
+		_listenForClick();
 	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.groups);
 		_showGroups();
 	}
 }

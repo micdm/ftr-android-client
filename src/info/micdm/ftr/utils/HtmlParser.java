5db25c761c1e7bbe4e1fa8440ee8eab88f1c62ab
Mic <mic-dm@mail.ru>
2011-10-01 16:13:38 +0700

    rmk:

--- src/info/micdm/ftr/utils/HtmlParser.java
+++ src/info/micdm/ftr/utils/HtmlParser.java
@@ -1,6 +1,11 @@
 package info.micdm.ftr.utils;
 
 import java.util.Date;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.util.Log;
 
 /**
  * Как бы парсер HTML-страниц.
@@ -17,7 +22,40 @@ public abstract class HtmlParser {
 		return probablyCorrect.getTime() > now.getTime() ? now : probablyCorrect;
 	}
 	
+	/**
+	 * Нормализует строку.
+	 */
 	protected String _normalizeString(String source) {
-		return source.trim().replace("&lt;", "<").replace("&gt;", ">").replace("&quot;", "\"").replace("&amp;", "&");
+		String result = source.trim();
+		//result = 
+		// TODO: удалять тэги
+		result = result.replace("&lt;", "<").replace("&gt;", ">").replace("&quot;", "\"").replace("&amp;", "&");
+		return result;
+	}
+	
+	/**
+	 * Возвращает шаблон для регулярного выражения.
+	 */
+	protected abstract String _getPattern();
+	
+	/**
+	 * Создает элемент из очередного набора сопоставленных данных.
+	 * Если возвращает false, сопоставление прекращается.
+	 */
+	protected abstract Boolean _createItem(MatchResult match);
+	
+	/**
+	 * Сопоставляет шаблон и строку.
+	 */
+	protected void _findMatches(String text) {
+		Log.d(toString(), "parsing text of length " + text.length());
+		Pattern pattern = Pattern.compile(_getPattern(), Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(text);
+		while (matcher.find()) {
+			if (!_createItem(matcher)) {
+				break;
+			}
+		}
+		Log.d(toString(), "text parsed");
 	}
 }

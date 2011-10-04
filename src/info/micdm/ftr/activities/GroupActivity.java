4eac8d9d807458466fcd4eed6937026e3ea9d0c3
Mic <mic-dm@mail.ru>
2011-10-04 22:46:37 +0700

    rmk: адаптер для группы теперь в отдельном классе

--- src/info/micdm/ftr/activities/GroupActivity.java
+++ src/info/micdm/ftr/activities/GroupActivity.java
@@ -4,100 +4,20 @@ import info.micdm.ftr.Forum;
 import info.micdm.ftr.Group;
 import info.micdm.ftr.R;
 import info.micdm.ftr.Theme;
+import info.micdm.ftr.adapters.GroupAdapter;
 import info.micdm.ftr.utils.Log;
 
 import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-
-/**
- * Адаптер для списка тем.
- * @author Mic, 2011
- *
- */
-class ThemeListAdapter extends BaseAdapter {
-	
-	/**
-	 * Класс для кэширования, как предлагают в официальных примерах.
-	 * @author Mic, 2011
-	 *
-	 */
-	static class ViewHolder {
-		TextView author;
-		TextView updated;
-		TextView title;
-	}
-	
-	/**
-	 * Этот товарищ будет создавать новый элемент из XML.
-	 */
-	protected LayoutInflater _inflater;
-	
-	/**
-	 * Список тем.
-	 */
-	protected ArrayList<Theme> _themes;
-	
-	public ThemeListAdapter(Context context, ArrayList<Theme> themes) {
-		_inflater = LayoutInflater.from(context);
-		_themes = themes;
-	}
-	
-	/**
-	 * Возвращает количество элементов.
-	 */
-	public int getCount() {
-		return _themes.size();
-	}
-	
-	/**
-	 * Возвращает элемент в указанной позиции.
-	 */
-	public Object getItem(int position) {
-		return _themes.get(position);
-	}
-
-	/**
-	 * Возвращает идентификатор элемента в указанной позиции.
-	 */
-	public long getItemId(int position) {
-		return position;
-	}
-
-	/**
-	 * Возвращает заполненный элемент списка.
-	 */
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder;
-		if (convertView == null) {
-			convertView = _inflater.inflate(R.layout.theme_list_item, null);
-			holder = new ViewHolder();
-			holder.author = (TextView)convertView.findViewById(R.id.themeAuthor);
-			holder.updated = (TextView)convertView.findViewById(R.id.themeUpdated);
-			holder.title = (TextView)convertView.findViewById(R.id.themeTitle);
-			convertView.setTag(holder);
-		} else {
-			holder = (ViewHolder)convertView.getTag();
-		}
-		Theme theme = (Theme)getItem(position);
-		holder.author.setText(theme.getAuthor());
-		holder.updated.setText(", " + theme.getUpdatedAsString());
-		holder.title.setText(theme.getTitle());
-		return convertView;
-	}
-}
 
 /**
  * Экран с темами внутри конкретной группы.
@@ -117,7 +37,7 @@ public class GroupActivity extends ListActivity {
 	protected void _onThemesAvailable(ArrayList<Theme> themes) {
 		// TODO: не пересоздавать адаптер
 		Log.debug(themes.size() + " themes available");
-		ThemeListAdapter adapter = new ThemeListAdapter(this, themes);
+		GroupAdapter adapter = new GroupAdapter(this, themes);
 		getListView().setAdapter(adapter);
 	}
 	

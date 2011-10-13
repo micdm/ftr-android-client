eb097bca296191d8d7c57ccec23948ef6344d263
Mic <mic-dm@mail.ru>
2011-10-13 22:43:49 +0700

    fix: иконки и название приложения

--- gen/info/micdm/ftr/R.java
+++ gen/info/micdm/ftr/R.java
@@ -33,6 +33,7 @@ public final class R {
         public static final int theme_list_item=0x7f030005;
     }
     public static final class string {
+        public static final int app_description=0x7f040001;
         public static final int app_name=0x7f040000;
     }
 }
